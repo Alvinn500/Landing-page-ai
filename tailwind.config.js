12f50207eabcb27090d66b8350b299d92a414eac
mochammadalvin <alfhin1212@gmail.com>
2023-06-24 19:53:38 +0700

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#7C26BF',
+        'accent' : '#E8B00E',
+      },
+      screens: {
+        '2xl' : '1320',
+      },
+    },
+  },
+  plugins: [],
+}
+
